3d88efc85719b203c3a180e21e3bd229629348bf
mamencode2 <mamencode2@gmail.com>
2024-02-29 14:42:00 +0000

    cvfd

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
+import { BottomNav } from "@/components/bottom-nav";
 import { HomeComp } from "@/components/home-comp";
 import { StoreFront } from "@/components/store-front";
 
 
 export default function Home() {
   return (
+    <>
     <StoreFront />
-
+    <BottomNav />
+    </>
   );
 }

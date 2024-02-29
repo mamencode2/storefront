72175e1604023d3963fe67cee8822bc2f2591284
mamencode2 <mamencode2@gmail.com>
2024-02-29 13:26:49 +0000

    cvfd

--- src/redux/StoreProvider.js
+++ src/redux/StoreProvider.js
@@ -1,7 +1,13 @@
 'use client'
-import { store } from './store'
+import { store, persistor } from './store'
 import { Provider } from 'react-redux'
+import { PersistGate } from "redux-persist/integration/react";
 
 export function StoreProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>
+    return <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            {children}
+        </PersistGate>
+
+    </Provider>
 }

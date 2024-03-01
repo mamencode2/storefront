0bc813e18b061f468ccaa5120afaf726b35da3e9
mamencode2 <mamencode2@gmail.com>
2024-03-01 16:09:56 +0000

    dg

--- src/app/wishlist/layout.jsx
+++ src/app/wishlist/layout.jsx
@@ -8,84 +8,84 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import WishListItems from '@/components/WishListItems';
 
-export default function WishListLayout({children}) {
-    const {savedItems}= useSelector((state)=> state.favItem)
+export default function WishListLayout({ children }) {
+  const { savedItems } = useSelector((state) => state.favItem)
   return (
     <div
-    style={{
-        padding:'8px 12px'
-    }}
+      style={{
+        padding: '8px 12px'
+      }}
     >
-<HeaderElement/>
-{savedItems.length>0?<WishListItems/>:<EmpetyWishList/>}
-        {children}
+      <HeaderElement />
+      {savedItems.length > 0 ? <WishListItems /> : <EmpetyWishList />}
+      {children}
     </div>
   )
 }
 
-function EmpetyWishList(){
-    return(
-        <ErrorBlock
-        image='https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg'
-        style={{
-          '--image-height': '150px',
-        }}
-        description={
-          <span>
-            Wish List Empety
-          </span>
-        }
-      >
-        <Button color='primary'>Shop Now</Button>
-      </ErrorBlock>
-    )
+function EmpetyWishList() {
+  return (
+    <ErrorBlock
+      image='https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg'
+      style={{
+        '--image-height': '150px',
+      }}
+      description={
+        <span>
+          Wish List Empety
+        </span>
+      }
+    >
+      <Button color='primary'>Shop Now</Button>
+    </ErrorBlock>
+  )
 }
-function HeaderElement(){
-    const router = useRouter()
+function HeaderElement() {
+  const router = useRouter()
 
-    const right = (
-        <div  >
-          <Space style={{ '--gap': '10px' }}>
-            < UnorderedListOutline fontSize={24}/>
-            <CartBadge/>
-          </Space>
-        </div>
-      )
-    return(
-        <NavBar
-        onBack={()=>{
-            router.back()
+  const right = (
+    <div  >
+      <Space style={{ '--gap': '10px' }}>
+        < UnorderedListOutline fontSize={24} />
+        <CartBadge />
+      </Space>
+    </div>
+  )
+  return (
+    <NavBar
+      onBack={() => {
+        router.back()
+      }}
+
+      right={right}
+    >
+      <h3
+        style={{
+          fontWeight: 'bold'
         }}
-
-        right={right}
-        >
-            <h3
-            style={{
-                fontWeight:'bold'
-            }}
-            >
-                WishList
-            </h3>
-        </NavBar>
-    )
+      >
+        WishList
+      </h3>
+    </NavBar>
+  )
 }
 function ShoppingCartIcon(props) {
-    return (
-        (<svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round">
-            <circle cx="8" cy="21" r="1" />
-            <circle cx="19" cy="21" r="1" />
-            <path
-                d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
-        </svg>)
-    );
+  return (
+    (<svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+      <circle cx="8" cy="21" r="1" />
+      <circle cx="19" cy="21" r="1" />
+      <path
+        d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
+    </svg>)
+  );
 }

5807f18ba7da5a911a479d7e6cc60c8b47c03eb6
mamencode2 <mamencode2@gmail.com>
2024-03-03 10:23:51 +0000

    dfg

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -9,3 +9,19 @@ export function currencyConvert(amount){
       let symbol = convertedCurr.slice(0,2)
     return  {symbol,curAmt}
 }
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function sliceArray(array, sliceSize) {
+  const slicedArrays = [];
+  for (let i = 0; i < array.length; i += sliceSize) {
+    const slice = array.slice(i, i + sliceSize);
+    slicedArrays.push(slice);
+  }
+  return slicedArrays;
+}
+export async function mockRequest(){
+
+}

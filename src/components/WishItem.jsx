7fbf84cae31a55177e23f647e5b8257eb88c4786
mamencode2 <mamencode2@gmail.com>
2024-03-03 16:23:18 +0000

    dfz

--- src/components/WishItem.jsx
+++ src/components/WishItem.jsx
@@ -2,7 +2,7 @@
 import { currencyConvert } from '@/utils/helper'
 import { Grid, Card, Image } from 'antd-mobile'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function WishItem({ item }) {
     return (
@@ -129,8 +129,49 @@ function PriceLable({product}){
 
                     </div>
                 </div>
-                <div className="" />
+                <QuickView product={product} />
            
                 </div>
     )
 }
+
+
+function ShoppingCartIcon(props) {
+    return (
+      (<svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round">
+        <circle cx="8" cy="21" r="1" />
+        <circle cx="19" cy="21" r="1" />
+        <path
+          d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
+      </svg>)
+    );
+  }
+
+
+  function QuickView({product}){
+    const [isOpen, setOpen]= useState(false)
+    return(
+        <>
+        <div className="" role='button'
+        style={{ 
+          border:'1px solid black',
+          padding:'2px 8px',
+          borderRadius:'6px'   
+        }}>
+        <ShoppingCartIcon/>
+        </div>
+
+        
+        </>
+    )
+  }

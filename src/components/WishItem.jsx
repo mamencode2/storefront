676021ba9af79c6c6ba951071951ef18a712aee3
mamencode2 <mamencode2@gmail.com>
2024-03-03 11:22:53 +0000

    dfsf

--- src/components/WishItem.jsx
+++ src/components/WishItem.jsx
@@ -1,4 +1,5 @@
 'use client'
+import { currencyConvert } from '@/utils/helper'
 import { Grid, Card, Image } from 'antd-mobile'
 import Link from 'next/link'
 import React from 'react'
@@ -62,8 +63,74 @@ export default function WishItem({ item }) {
                             {item.goods_name}
                         </span>
                     </h1>
+                    
                 </div>
+
+                <PriceLable
+product={item}
+                    />
             </div>
         </Grid.Item>
+    )
+}
+
+
+function PriceLable({product}){
+    let salePrice = currencyConvert(Number(product.salePrice.amount))
+    let retailPrice = currencyConvert(Number(product.retailPrice.amount))
+    return(
+        <div
+                className=""
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    padding: '0 8px'
+                }}
+            >
+        <div className=""
+                style={{
+                    display:'flex',
+                    alignItems:'center'
+                }}
+                >
+                    <span
+                        style={{
+                            fontSize: "17px",
+                            fontWeight: "600",
+                            color: Number(product.salePrice.amount) < Number(product.retailPrice.amount) ? "#fa6338" : '',
+                        }}
+                    >
+                        {salePrice.curAmt}
+                    </span>
+                    {" "}
+                    <span
+                        style={{
+                            fontSize: "12px",
+                        }}
+                    >
+                        ብር
+                    </span>
+                    <div className=""
+                    style={{
+                        marginLeft:'8px'
+                    }}
+                    >
+                    <span
+                        style={{
+                            fontSize: "12px",
+                            textDecoration:'line-through'
+                        }}
+                    >
+                        {retailPrice.curAmt}
+                        {' '}
+                        ብር
+                    </span>
+
+                    </div>
+                </div>
+                <div className="" />
+           
+                </div>
     )
 }

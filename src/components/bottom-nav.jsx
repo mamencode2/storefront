0e13c8fe59e46f5eda9b4e3ff9697ed4d4274b3b
mamencode2 <mamencode2@gmail.com>
2024-03-04 18:27:42 +0000

    dfr

--- src/components/bottom-nav.jsx
+++ src/components/bottom-nav.jsx
@@ -27,7 +27,7 @@ let bottomNavs = [
   {
     label: "Cart",
     href: "/cart",
-    icon: <CartBadge />
+    icon: <ShoppingCartIcon className="h-6 w-6"/>
   },
   {
     label: "Profile",
@@ -56,7 +56,7 @@ const activePath = pathname === '/' || pathname === '/home' ? '/' : pathname;
           <Link
             className="flex flex-1 flex-col items-center justify-center py-2 text-sm text-gray-500 dark:text-gray-400"
             style={{
-              color: activePath === link.href? "black":"",
+              color: activePath === link.href? "black":"#767676",
               fontWeight: activePath === link.href?"bold":""
             }}
             href={link.href}>
@@ -167,7 +167,26 @@ function PenToolIcon(props) {
   );
 }
 
-
+function ShoppingCartIcon(props) {
+  return (
+      (<svg
+          {...props}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round">
+          <circle cx="8" cy="21" r="1" />
+          <circle cx="19" cy="21" r="1" />
+          <path
+              d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
+      </svg>)
+  );
+}
 function ShoppingBagIcon(props) {
   return (
     (<svg

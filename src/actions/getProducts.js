7a4061a4e236d7113c08471c1c787f03daec733d
mamencode2 <mamencode2@gmail.com>
2024-03-06 13:57:08 +0000

    xdc

--- src/actions/getProducts.js
+++ src/actions/getProducts.js
@@ -1,36 +1,45 @@
-'use server'
+"use server";
 
-import storwithD from "@/data/storwithD"
+import storwithD from "@/data/storwithD";
+import { cookies,headers } from "next/headers";
+import { NextResponse } from "next/server";
+export const getStoreData = async (storeCode) => {
+  console.log(storeCode);
+  try {
+    let storeData = storwithD.find(
+      (item) => `${item.store.storeCode}` === `${storeCode}`
+    );
 
-export const getStoreData= async (storeCode)=>{
-    console.log(storeCode)
-try {
-    let storeData = storwithD.find((item)=> `${item.store.storeCode}` ===`${storeCode}`)
+    return storeData;
+  } catch (error) {
+    throw new Error(`An error happend:${error}`);
+  }
+};
+export const getProduct = async (key) => {
+  try {
+    let product = storwithD
+      .map((sdata) => sdata.pItems)
+      .flat()
+      .find((item) => item.key === key);
 
-    return storeData
-} catch (error) {
-    throw new Error(`An error happend:${error}`)
-}
-}
-export const getProduct= async(key)=>{
-try {
-    let product = storwithD.map((sdata) => sdata.pItems).flat().find((item)=> item.key === key)
+    return product;
+  } catch (error) {
+    throw new Error(`An error happend:${error}`);
+  }
+};
 
-    return product
-} catch (error) {
-    throw new Error(`An error happend:${error}`)
-}
-}
+export const getProducts = async (offset, limit, requestType, page) => {
+  console.log(requestType, page);
+  
+  try {
+    let products = storwithD.map((sdata) => sdata.pItems).flat();
+    let responseProducts = products.slice(offset, offset + limit);
 
-export const getProducts = async (offset, limit,requestType,page) => {
-    console.log(requestType,page)
-    try {
-        
-        let products = storwithD.map((sdata) => sdata.pItems).flat()
-        let responseProducts = products.slice(offset, offset+limit)
-
-        return responseProducts
-    } catch (error) {
-        throw new Error(`An error happend:${error}`)
-    }
-}
+    return responseProducts
+      
+      
+    
+  } catch (error) {
+    throw new Error(`An error happend:${error}`);
+  }
+};

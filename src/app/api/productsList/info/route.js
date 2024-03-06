23649d92af9324be2537cbc390429d026c19b8fb
mamencode2 <mamencode2@gmail.com>
2024-03-06 14:13:55 +0000

    ds

--- src/app/api/productsList/info/route.js
+++ src/app/api/productsList/info/route.js
@@ -1,9 +1,18 @@
 import { NextRequest,NextResponse } from "next/server";
 import { cookies,headers } from "next/headers";
+import storwithD from "@/data/storwithD";
 
 export async function GET(request){
+    const searchParams = request.nextUrl.searchParams
+    let request_type = searchParams.get('request_type')
+    let limit = searchParams.get('limit')
+    let page = searchParams.get('page')
+
+    console.log(request_type,limit,page)
 try {
-    console.log(request)
+    let products = storwithD.map((sdata) => sdata.pItems).flat();
+    //console.log(request)
+    console.log(products.length)
     return NextResponse.json({
         msg:"ok",
         status:200
